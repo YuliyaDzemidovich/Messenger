285d00b4354ca873956727d20bfda72d4fbc9571
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2022-12-22 23:44:43 +0400

    converted to Spring Boot and Maven project

---
+++ src/test/java/example/ydzemidovich/messenger/MessengerApplicationTests.java
@@ -0,0 +1,16 @@
+package example.ydzemidovich.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+	/**
+	 * Sanity test that Spring Boot can load its context
+	 */
+	@Test
+	void contextLoads() {
+	}
+
+}
